bdd0d6f2ec99d107fa448d3787a8afdca66e4024
Lunatic-v5 <150667950+Lunatic-v5@users.noreply.github.com>
2023-11-21 22:49:07 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,6 +27,7 @@ wget -O /tmp/menu.zip "${REPO}config/menu.zip" >/dev/null 2>&1
     7z e  /tmp/menu.zip -o/tmp/menu/ >/dev/null 2>&1
     chmod +x /tmp/menu/*
     mv /tmp/menu/* /usr/sbin/
+    echo "ketik done [ Lalu enter ] "
 echo -e " [INFO] Update Successfully" | lolcat
 mkdir -p /etc/ssh
 mkdir -p /etc/vmx

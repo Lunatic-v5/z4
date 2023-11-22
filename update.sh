54dc50177d792d929fb2ae96ce039086265919d9
Lunatic-v5 <150667950+Lunatic-v5@users.noreply.github.com>
2023-11-22 13:58:20 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
@@ -7,35 +6,49 @@ REPO="https://raw.githubusercontent.com/Lunatic-v5/z4/Lunatic/"
 CYAN='\033[0;96m'        # Cyan
 
 BG="\E[44;1;39m"
+
 NC="\E[0m"
+
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+
 echo -e "  ${BG} >>> INFORMASI UPDATE SCRIP <<< ${NC}"
+
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+
 echo -e "      •> Add UDP Server <•" | lolcat
+
 echo -e "    •> Install UDP Server <•" | lolcat
+
 echo -e "      •> Fix Bug Script <•" | lolcat
-echo -e "     •> Script By @Lunatic <•" | lolcat
+
+echo -e "     •> Script By @Lunatic<•" | lolcat
+
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "  ${BG} >>> THANKS TO LUNATIC <<< ${NC}" 
+
+echo -e "  ${BG} >>> THANKS TO LUNATIC<<< ${NC}" 
+
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+
 sleep 3
-clear
-echo "Ketik Apt [ Lalu Enter ] " | lolcat
-apt
-sleep 3
+echo -e " [INFO] Downloading Update File"
+sleep 2
 rm -rf /tmp/menu
-wget -O /tmp/menu.zip "${REPO}config/menu.zip" >/dev/null 2>&1
+wget -O /tmp/menu-master.zip "${REPO}config/menu.zip" >/dev/null 2>&1
     mkdir /tmp/menu
-  #  chmod +x /tmp/menu
-    7z e  /tmp/menu.zip -o/tmp/menu/ >/dev/null 2>&1
+    7z e  /tmp/menu-master.zip -o/tmp/menu/ >/dev/null 2>&1
     chmod +x /tmp/menu/*
     mv /tmp/menu/* /usr/sbin/
-echo -e " [INFO] Update Successfully" | lolcat
+
+echo -e " [INFO] Update Menu Succes"
 mkdir -p /etc/ssh
 mkdir -p /etc/vmx
 mkdir -p /etc/vls
 mkdir -p /etc/tr
 mkdir -p /etc/ss
+echo -e " [INFO] Installing SSH UDP"
+wget https://raw.githubusercontent.com/Lunatic-v5/z4/Lunatic/backup/set-br.sh
+bash set-br.sh
+
 rm -rf update.sh
 sleep 2
 read -n 1 -s -r -p "Press [ Enter ] to back on menu"
